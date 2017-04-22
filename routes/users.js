78da9a47314793a90befb60fd5ba7f25024153e3
LG-53\Administrator <qjadud22@naver.com>
2017-04-23 02:24:29 +0900

    order Front Page Finish & Daum address API

--- routes/users.js
+++ routes/users.js
@@ -20,9 +20,6 @@ module.exports = function(conn){
   });
 
   router.post('/cartAdd',function(req,res){
-
-
-
       conn.query(sql,cart,function(err,rows){
         if(err){
           console.log(err);
@@ -33,14 +30,13 @@ module.exports = function(conn){
   });
 
   router.get('/order',function(req,res){
-
     var user_num = req.user.user_num;
     console.log('order:'+user_num);
     var sql ='SELECT c.cartno,c.user_num,c.pboardno,c.size,c.color,c.productcount,c.price,p.mainimage,p.productname FROM cart c join product_board b join products p where c.user_num = ? and c.pboardno = b.pboardno and b.pno = p.pno';
     conn.query(sql,[user_num],function(err,rows){
-
+          res.render('order',{rows:rows});
     });
-      res.render('order');
+
   });
 
   return router;
