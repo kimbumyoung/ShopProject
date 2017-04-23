60ce0a69b87f72ec8862f5ffcd8626255cfa4baa
LG-53\Administrator <qjadud22@naver.com>
2017-04-23 23:18:59 +0900

    product buy process complete

--- routes/users.js
+++ routes/users.js
@@ -32,12 +32,73 @@ module.exports = function(conn){
   router.get('/order',function(req,res){
     var user_num = req.user.user_num;
     console.log('order:'+user_num);
-    var sql ='SELECT c.cartno,c.user_num,c.pboardno,c.size,c.color,c.productcount,c.price,p.mainimage,p.productname FROM cart c join product_board b join products p where c.user_num = ? and c.pboardno = b.pboardno and b.pno = p.pno';
+    var sql ='SELECT c.cartno,c.user_num,c.pboardno,c.size,c.color,c.productcount,c.price,p.mainimage,p.productname,b.pno FROM cart c join product_board b join products p where c.user_num = ? and c.pboardno = b.pboardno and b.pno = p.pno';
     conn.query(sql,[user_num],function(err,rows){
-          res.render('order',{rows:rows});
+      if(err){
+        console.log(err);
+      }else{
+        var sqlPrice = 'select sum(price) as price from cart where user_num = ?';
+        conn.query(sqlPrice,[user_num],function(err,price){
+          res.render('order',{rows:rows,totalPrice:price});
+        });
+      }
     });
-
   });
 
+  router.post('/orderProductAdd',function(req,res){
+      var a = req.body.pno.split(',');
+
+      for(var i =0; i<a.length; i++){
+        var orderdetail = {
+          orderDetailno : req.body.orderDetailno,
+          user_num : req.body.user_num,
+          pno : a[i]
+        };
+        b(orderdetail);
+      }
+
+      res.send('success');
+  });
+
+
+  router.post('/order',function(req,res){
+    var order = {
+      user_num : req.body.user_num,
+      orderName : req.body.orderName,
+      orderPhone : req.body.orderPhone,
+      orderEmail : req.body.orderEmail,
+      orderAddress : req.body.orderAddress,
+      orderDetailno : req.body.orderDetailno,
+      orderMessage : req.body.orderMessage
+    };
+    var sql = 'insert into ordertable set ?';
+    conn.query(sql,order,function(err,rows){
+        if(err){
+          console.log(err);
+        }else{
+          var user_num = req.body.user_num;
+          var sql = 'delete from cart where user_num = ? ';
+          conn.query(sql,[user_num],function(err,rows){
+              if(err){
+                console.log(err);
+              }else{
+                res.send('success');
+              }
+          });
+        }
+    });
+  });
+  router.get('/orderComplete',function(req,res){
+    res.render('orderComplete');
+  });
+
+
+
+var b = function sqlAdd(orderdetail){
+    console.log(orderdetail);
+    var sql = 'insert into orderdetail set ?';
+    conn.query(sql,orderdetail,function(err,rows){
+    });
+  };
   return router;
 };
