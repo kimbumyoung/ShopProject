03984d5dbedc01abd9e4015a3986555032e66e4d
kkk\k <qjadud22@naver.com>
2017-04-28 21:06:34 +0900

    mypage orderlist read page

--- routes/users.js
+++ routes/users.js
@@ -79,7 +79,6 @@ module.exports = function(conn){
       totalCount : req.body.totalCount,
       totalPrice : req.body.totalPrice,
       productHead : req.body.productHead
-
     };
     var sql = 'insert into ordertable set ?';
     conn.query(sql,order,function(err,rows){
@@ -97,7 +96,7 @@ module.exports = function(conn){
           });
         }
     });
-    
+
   });
   router.get('/orderComplete',function(req,res){
     res.render('orderComplete');
@@ -106,17 +105,46 @@ module.exports = function(conn){
   router.get('/mypage',function(req,res){
       res.render('mypage');
   });
+
+  Date.prototype.toFormatString = function(format) {
+      var year = this.getFullYear();
+      var month = this.getMonth() + 1;
+      var day = this.getDate();
+      var hour = this.getHours();
+      var minute = this.getMinutes();
+      var second = this.getSeconds();
+
+      if (format === null) format = "yyyy-MM-dd";
+      format = format.replace("yyyy", year);
+      format = (month < 10) ? format.replace("MM", "0" + month) : format.replace("MM", month);
+      format = format.replace("M", month);
+      format = (day < 10) ? format.replace("dd", "0" + day) : format.replace("dd", day);
+      format = format.replace("d", day);
+      format = (hour < 10) ? format.replace("HH", "0" + hour) : format.replace("HH", hour);
+      format = (minute < 10) ? format.replace("mm", "0" + minute) : format.replace("mm", minute);
+      format = (second < 10) ? format.replace("ss", "0" + second) : format.replace("ss", second);
+      return format;
+};
   router.get('/mypage/orderList',function(req,res){
     var user_num =  req.user.user_num ;
     var sql ='select * from ordertable where user_num =?';
+    var array=[];
     conn.query(sql,[user_num],function(err,rows){
           if(err){
             console.log(err);
           }else{
-            res.render('orderList',{rows:rows});
+              for(var i=0;i<rows.length; i++){
+                var date = new Date([rows[i].orderdate]);
+                  array[i]= date.toFormatString("yyyy-MM-dd");
+              }
+            res.render('orderList',{rows:rows,date:array});
           }
     });
   });
+
+
+
+
 
 var b = function sqlAdd(orderdetail){
     console.log(orderdetail);
